2f536fa8a274857052801c1147f6add46551a3de
Siffer <siffer331@gmail.com>
2020-03-06 09:23:08 +0100

    closeable dialogboxes

--- s1m/scripts/npcDialog.gd
+++ s1m/scripts/npcDialog.gd
@@ -5,6 +5,7 @@ onready var Player : Node2D = get_parent().get_parent().get_node("Player")
 
 var dialog: Array = ["This is text.", "This is page 2 :D", "Goodbye!"]
 var page: int = 0
+var away : bool = true
 
 
 func _ready() -> void:
@@ -14,11 +15,13 @@ func _process(delta):
 	var pos : Vector2 = get_parent().position;
 	
 	if pos.distance_squared_to(Player.position) < 10000:
-		if Player.dialogShowing == false:
+		if !Player.dialogShowing && away:
 				Player.dialogShowing = true
 				showDialog()
+				away = false
 	else:
 			Player.dialogShowing = false
+			away = true
 			hideDialog()
 
 
@@ -39,9 +42,10 @@ func _input(event) -> void:
 				page += 1
 				set_bbcode(dialog[page])
 				set_visible_characters(0)
+			else:
+				hideDialog()
 		else:
 			set_visible_characters(get_total_character_count())
-	
 
 
 func _on_Timer_timeout() -> void:
