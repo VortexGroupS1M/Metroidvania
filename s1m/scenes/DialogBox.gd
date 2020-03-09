f1ad050eb7bdda3eea8149a06c7946b7100419f2
Siffer <siffer331@gmail.com>
2020-03-09 11:17:17 +0100

    Small changes to Dialog system

---
+++ s1m/scenes/DialogBox.gd
@@ -0,0 +1,3 @@
+extends Node2D
+export(int) var squareRange = 10000 
+export(Array) var dialog: Array = ["This is text.", "This is page 2 :D", "Goodbye!"]
--- s1m/scripts/npcDialog.gd
+++ s1m/scripts/npcDialog.gd
@@ -1,9 +1,9 @@
 extends RichTextLabel
 
-onready var Player : Node2D = get_parent().get_parent().get_node("Player")
+onready var Player : Node2D = get_parent().get_parent().get_parent().get_node("Player")
 #onready var Dummy = get_parent().get_parent().get_node("Dummy")
 
-var dialog: Array = ["This is text.", "This is page 2 :D", "Goodbye!"]
+onready var dialog: Array = get_parent().get_parent().dialog
 var page: int = 0
 var away : bool = true
 
@@ -12,9 +12,9 @@ func _ready() -> void:
 	get_parent().hide()
 
 func _process(delta):
-	var pos : Vector2 = get_parent().position;
+	var pos : Vector2 = get_parent().get_parent().position;
 	
-	if pos.distance_squared_to(Player.position) < 10000:
+	if pos.distance_squared_to(Player.position) < get_parent().get_parent().squareRange:
 		if !Player.dialogShowing && away:
 				Player.dialogShowing = true
 				showDialog()
